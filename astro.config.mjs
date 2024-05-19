717bcdbe99b42cd6eade1161defc41a90f8bef39
EdericoDev <federicobarbera455@gmail.com>
2024-05-19 19:33:10 +0200

    temporary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,33 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+ 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://www.kozhuhds.com',
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [
+        /**
+         * Adds ids to headings
+         */
+        rehypeSlug,
+        [
+          /**
+           * Enhances code blocks with syntax highlighting, line numbers,
+           * titles, and allows highlighting specific lines and words
+           */
+ 
+          rehypePrettyCode,
+          {
+            theme: 'github-dark',
+          },
+        ],
+      ],
+    }),
+  ],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,mdx}', 
+  ],
+  theme: {
+    // ...
+  },
+  plugins: [
+    typography,
+    // ...
+  ],
+}
