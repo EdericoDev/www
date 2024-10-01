12a872cf085d6e3a5dc928679f5b5733ed6feaf5
EdericoDev <federicobarbera455@gmail.com>
2024-10-01 18:11:09 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeSlug from 'rehype-slug';
-import astroIcon from 'astro-icon';
-
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://ederico.dev',
-  integrations: [
-    [astroIcon()],
-    tailwind(),
-    mdx({
-      syntaxHighlight: false,
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: 'github-dark',
+export default {
+    vite: {
+      css: {
+        preprocessorOptions: {
+          css: {
+            additionalData: '@import "src/styles/global.css";',
           },
-        ],
-      ],
-    }),
-  ],
-  content: {
-    collections: {
-      blog: {
-        schema: {
-          title: 'string',
-          description: 'string',
-          date: 'date',
-          slug: 'string',
         },
       },
     },
-  },
-});
+  };
+  
+  
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{astro,css,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#ff9900',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
