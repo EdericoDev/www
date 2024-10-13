a97f62dbb912d892ba2303ff374f11f661112884
EdericoDev <hello@ederico.dev>
2024-10-13 22:45:34 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import astroIcon from 'astro-icon';
 import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel/serverless'; 
+import vercel from '@astrojs/vercel/serverless';
+
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind(), astroIcon(), svelte()],
+  integrations: [tailwind(), astroIcon(), svelte(), mdx()],
   output: 'server',
   adapter: vercel(), 
 });
