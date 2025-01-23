ee35c437513aac36fc6f018dbf13371573e92af7
EdericoDev <hello@ederico.dev>
2025-01-23 16:28:00 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import astroIcon from 'astro-icon';
 import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [tailwind(), astroIcon(), svelte(), mdx()],
   output: 'server',
-  adapter: vercel(), 
+  adapter: vercel({
+    analytics: true,
+    maxDuration: 60
+  }),
 });
