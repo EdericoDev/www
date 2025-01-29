19f308ceda0435e566d09eb2196858c2e08ee8e2
EdericoDev <hello@ederico.dev>
2025-01-29 15:40:50 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import astroIcon from 'astro-icon';
+import icon from 'astro-icon';
 import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind(), astroIcon(), svelte(), mdx()],
+  integrations: [
+    tailwind(),
+    icon(),
+    svelte(),
+    mdx()
+  ],
   output: 'server',
-  adapter: vercel({
-    functionPerRoute: true,
-    webAnalytics: { enabled: true }
-  }),
+  adapter: vercel(),
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['@astrojs/icon']
+      }
+    },
+    ssr: {
+      noExternal: ['@astrojs/icon']
+    }
+  }
 });
