0af47fb5cc380aa89bf1f4b43166d827217347fa
EdericoDev <hello@ederico.dev>
2025-04-09 18:59:58 +0200

    test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
-// tailwind.config.cjs
-module.exports = {
-    content: [
-      './src/**/*.{astro,html,js,jsx,ts,tsx,mdx,svelte}',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'],
-        },
-      },
-    },
-    plugins: [],
-  }
-  
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
