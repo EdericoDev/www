6bdeec7b6bb33d80c7ddb22328a16d6c536bbc0d
EdericoDev <hello@ederico.dev>
2025-04-09 18:42:43 +0200

    test

---
+++ src/components/MyCounter.svelte
@@ -0,0 +1,8 @@
+<script>
+    let count = 0;
+  </script>
+  
+  <button on:click={() => count++} class="bg-blue-500 text-white px-4 py-2 rounded">
+    Hai cliccato {count} volte
+  </button>
+  
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.cjs
+module.exports = {
+    content: [
+      './src/**/*.{astro,html,js,jsx,ts,tsx,mdx,svelte}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
